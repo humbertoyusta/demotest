5f4203c5ea837680ef6745229a6ae5efd51ea507
Kirill Starkov <starkov.kirill123@gmail.com>
2024-10-28 14:31:29 +0800

    add docker and pg

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ["POSTGRES_HOST"],
+        'NAME': os.environ["POSTGRES_DB"],
+        'USER': os.environ["POSTGRES_USER"],
+        'PORT': os.environ["POSTGRES_PORT"],
+        'PASSWORD': os.environ["POSTGRES_PASSWORD"],
     }
 }
 
