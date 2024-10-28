4fdd9c5a595c4d8a239d4778f5ccb121578a58e1
Kirill Starkov <starkov.kirill123@gmail.com>
2024-10-28 14:32:27 +0800

    agent progress f1

--- blog/models.py
+++ blog/models.py
@@ -10,9 +10,10 @@ STATUS = (
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
-    updated_on = models.DateTimeField(auto_now= True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
+    short_description = models.CharField(max_length=255, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
@@ -21,4 +22,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
